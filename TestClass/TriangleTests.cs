9cf118a4ae004d98776ab4ca6105ec7e5ae6b6fb
Akshat Arora <akshat.arora456@gmail.com>
2023-06-05 15:59:01 -0400

    created first test case

---
+++ TestClass/TriangleTests.cs
@@ -0,0 +1,27 @@
+﻿using TriangleSolver;
+using NUnit.Framework;
+
+namespace TestClass
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void ValidTriangle_Input20and20and20_OutputEquilateralTriangle()
+        {
+            //Arrange
+            int firstSide = 20;
+            int secondSide = 20;
+            int thirdSide = 20;
+
+            string expected = "An EQUILATERAL triangle is formed";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide); 
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
